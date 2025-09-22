2d4eff852e086fa358dafba14216772b4fcc8382
Laura-West <43360406+Laura-West@users.noreply.github.com>
2025-09-22 12:31:41 +1000

    Update

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// Default settings. This file will be updated when you save changes from the Admin Dashboard.
+// Widget configuration file
+// This file will be updated when you save changes from the Admin Dashboard
 const widgetConfig = {
   theme: 'gilded-iris-theme',
   alignment: 'center'
